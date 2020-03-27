98eaa9e8a3845dd31a5b9d64095106a6b4f3dd38
ofentsesambo <ofentsesambo@gmail.com>
2020-03-27 14:49:01 +0200

    first commit

---
+++ table.sql
@@ -0,0 +1,10 @@
+create table visitor
+(   id seial primar key,
+    VisitorName varchar(100),
+    age integer,
+    dateOfVisit date,
+    timeOfVisit time,
+    assistantName varchar(100)
+    comments varchar(225)
+);
+
