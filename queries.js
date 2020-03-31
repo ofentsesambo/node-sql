566811e268a0f9d8f17950d19630439c499606fa
ofentsesambo <ofentsesambo@gmail.com>
2020-03-31 10:57:20 +0200

    commit without tests

--- queries.js
+++ queries.js
@@ -29,3 +29,49 @@ const addNewVisitor = async (name, age, dateOfVisit, timeOfVisit, assistantName,
         throw error;
     }
 };
+
+const listAllVisiors = async() => {
+    const SQL = `SELECT id, visitorName FROM visitor;`
+    try {
+        query = await client.query(SQL);
+        return query.rows;
+    } catch(error){
+        throw error;
+    }
+};
+
+const deleteVisitor = async(id) => {
+    try {
+        let deleted = await client.query(`DELETE FROM visitor where id =$1;`,[id]);
+        return deleted;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateVistor = async(id, where, value) => {
+    try {
+        let updated = await client.query(`UPDATE visitor set ${where}= $2 WHERE id = $1;` [id,value]);
+        return updated;
+    } catch (error){
+        throw error;
+    }
+};
+
+const viewVisitor = async(id) => {
+    try {
+        const query = await client.query(`SELECT * FROM visitor WHERE id=$1;`,[id]);
+        return query.rows;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteVisitor = async() => {
+    try {
+        let emptied = await client.query(`DELETE FROM viewVisitor;`);
+        return deleted;
+    } catch(error) {
+        throw error;
+    }
+};
